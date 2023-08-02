797d0e54f529ce03a05d7ee72309d532bc298eee
John Boi <jon0908776@gmail.com>
2023-08-02 15:31:01 -0400

    Portfolio and about page re-done

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,27 +2,46 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div className='container top-buffer bot-buffer'>
-      <h2 className='text-center'>About Me</h2>
-      <div className='m-auto'>
+    <div className='container top-buffer bot-buffer about-container'>
+      <h2 className='text-center'>Professional Life</h2>
+      <div className='m-auto d-flex align-items-center about-section'>
         <img
           className='d-block mx-auto'
-          alt="My face"
-          src={require('../../images/me.jpg')}
+          alt="Professional Shot"
+          src={require('../../images/ProfessionalCropped.jpg')}
+          width="200px"
+        />
+        <div className="ml-3 about-text">
+          <p>
+            My name is John Medeiros and I am a motivated and independent software developer with a background in computer engineering.
+            I am a strong problem solver with fantastic analytical skills and a drive for continuous learning and exploring new solutions.
+            I have a proven ability to design, implement and validate technical solutions. I have excellent communication skills and use them to
+            effectively collaborate with stakeholders and convey technical concepts. I am an adaptable team player with a track record of
+            delivering commercial software.
+          </p>
+          <p>
+            I am currently taking interest in data science and machine learning with Python and TensorFlow. My sights are set on
+            passing the TensorFlow developer certification exam from Google by the Fall. I believe that data is the future.
+          </p>
+        </div>
+      </div>
+      <h2 className='text-center top-buffer'>Free Time</h2>
+      <div className='m-auto d-flex align-items-center about-section'>
+        <div className="mr-3 about-text">
+          <p>
+            In my free time you can usually find me out on the golf courses around the Burlington/Kitchener/Cambridge areas. My goal this year is to break 100.
+            I enjoy hanging out with my golf buddies, having a beer on the course and trying my best to make a few pars. If I am not golfing then there is a good chance I am playing games online.
+            I play quite a large variety of games, which you can read about if you are interested in the games section. (I consider myself pretty tough to take down in Smash Ultimate.)
+            I also love card games, board games, ping-pong, and pool.
+          </p>
+        </div>
+        <img
+          className='d-block mx-auto'
+          alt="Golf Shot"
+          src={require('../../images/GolfCropped.jpg')}
           width="200px"
         />
       </div>
-      <p className='text-center'>
-        My name is John Medeiros and I am a motivated and independant software developer with a background in computer engineering.
-        I am a strong problem solver with fantastic analytical skills and a drive for continuous learning  and exploring new solutions.
-        I have a proven ability to design, implement and validate technical solutions. I have excellent communication skills and use them to
-        effectively collaberate with stakeholders and convey technical concepts. I am an adaptable team player with a track record of
-        delivering commercial software.
-      </p>
-      <p className='text-center'>
-        I am currently taking interest in data science and machine learning with python and TensorFlow. My sights are set on
-        passing the TensorFlow developer certification exam from Google by the fall. I believe that data is the future.
-      </p>
     </div>
   );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,76 +3,35 @@ import './Portfolio.css'
 
 export default function Portfolio() {
   return (
-    <div className='bot-buffer'>
+    <div className='bot-buffer top-buffer'>
       <div className="row justify-content-center">
+      <h2 className='text-center'>WebDev Projects</h2>
       <div class="flip-card-container">
         <div class="flip-card">
           <div class="card-front">
             <figure>
               <div class="img-bg"></div>
-              <img src={require('../../images/BoostRocket.png')} alt="Boost Rocket"/>
-              <figcaption>Boost Rocket</figcaption>
+              <img src={require('../../images/phrases.png')} alt="PositivePhrases"/>
+              <figcaption>Positive Phrases</figcaption>
             </figure>
 
             <h3>Technologies:</h3>
-            <p>Unity Version 2021.3.18f1</p>
-            <p>C#</p>
-            <p>OOP</p>
+            <p>React</p>
+            <p>Express</p>
+            <p>NodeJS</p>
+            <p>NodeMailer</p>
+            <p>MySQL</p>
+            <p>Sequelize</p>
           </div>
 
           <div class="card-back">
             <figure>
               <div class="img-bg"></div>
-              <img src={require('../../images/Boost2.png')} alt="Boost Rocket2"/>
+              <img src={require('../../images/quote.jpg')} alt="positivequote"/>
             </figure>
 
-            <a href='https://sharemygame.com/@jon0908776/boostrocket'>
-              <button>Play it here!</button>
-            </a>
-            <a href='https://github.com/JWMedeiros/Unity_Games/tree/master/BoostRocket/Assets'>
-              <button className='top-buffer'>Asset Library</button>
-            </a>
-
-            <div class="design-container">
-              <span class="design design--1"></span>
-              <span class="design design--2"></span>
-              <span class="design design--3"></span>
-              <span class="design design--4"></span>
-              <span class="design design--5"></span>
-              <span class="design design--6"></span>
-              <span class="design design--7"></span>
-              <span class="design design--8"></span>
-            </div>
-          </div>
-
-        </div>
-      </div>
-      <div class="flip-card-container">
-        <div class="flip-card">
-          <div class="card-front">
-            <figure>
-              <div class="img-bg"></div>
-              <img src={require('../../images/GunGame.png')} alt="Gun Game"/>
-              <figcaption>GunGame</figcaption>
-            </figure>
-
-            <h3>Technologies:</h3>
-            <p>Unity Version 2021.3.18f1</p>
-            <p>C#</p>
-            <p>OOP</p>
-          </div>
-
-          <div class="card-back">
-            <figure>
-              <div class="img-bg"></div>
-              <img src={require('../../images/Gun2.jpg')} alt="GunGame2"/>
-            </figure>
-
-            <a href='https://sharemygame.com/@jon0908776/gungame-1-0'>
-              <button>Play it here!</button>
-            </a>
-            <a href='https://github.com/JWMedeiros/Unity_Games/tree/master/GunGame/Assets'>
-              <button className='top-buffer'>Asset Library</button>
+            <a href='https://github.com/JWMedeiros/positive_phrases'>
+              <button>Github</button>
             </a>
 
             <div class="design-container">
@@ -306,6 +265,273 @@ export default function Portfolio() {
             </div>
           </div>
 
+        </div>
+      </div>
+      </div>
+      <div className="row justify-content-center">
+      <h2 className='text-center'>Data Science/ML Projects</h2>
+      <div class="flip-card-container">
+        <div class="flip-card">
+          <div class="card-front">
+            <figure>
+              <div class="img-bg"></div>
+              <img src={require('../../images/bitpredict.png')} alt="BitPredict"/>
+              <figcaption>BitPredict</figcaption>
+            </figure>
+
+            <h3>Technologies:</h3>
+            <p>Python</p>
+            <p>TensorFlow</p>
+            <p>Forecasting</p>
+            <p>Numpy</p>
+            <p>Pandas</p>
+            <p>Matplotlib</p>
+          </div>
+
+          <div class="card-back">
+            <figure>
+              <div class="img-bg"></div>
+              <img src={require('../../images/demand-forecasting.jpg')} alt="FV2"/>
+            </figure>
+
+            <a href='https://colab.research.google.com/drive/1NK3rb99J-yvcTEDOzlkpXJUYf-v8YUPj?usp=sharing'>
+              <button>Link to Colab notebook</button>
+            </a>
+
+            <div class="design-container">
+              <span class="design design--1"></span>
+              <span class="design design--2"></span>
+              <span class="design design--3"></span>
+              <span class="design design--4"></span>
+              <span class="design design--5"></span>
+              <span class="design design--6"></span>
+              <span class="design design--7"></span>
+              <span class="design design--8"></span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="flip-card-container">
+        <div class="flip-card">
+          <div class="card-front">
+            <figure>
+              <div class="img-bg"></div>
+              <img src={require('../../images/SkimLit.jpg')} alt="SkimLit"/>
+              <figcaption>SkimLit</figcaption>
+            </figure>
+
+            <h3>Technologies:</h3>
+            <p>Python</p>
+            <p>TensorFlow</p>
+            <p>Natural Language Processing</p>
+            <p>Numpy</p>
+            <p>Pandas</p>
+            <p>Matplotlib</p>
+          </div>
+
+          <div class="card-back">
+            <figure>
+              <div class="img-bg"></div>
+              <img src={require('../../images/sl2.jpg')} alt="skimlit2"/>
+            </figure>
+
+            <a href='https://colab.research.google.com/drive/1mDeczIo4xMHmrnY-LxUlxFS6bytWgxBt?usp=sharing'>
+              <button>Link to Colab notebook</button>
+            </a>
+            <a href='https://arxiv.org/abs/1612.05251'>
+              <button className='top-buffer'>Original SkimLit Paper</button>
+            </a>
+
+            <div class="design-container">
+              <span class="design design--1"></span>
+              <span class="design design--2"></span>
+              <span class="design design--3"></span>
+              <span class="design design--4"></span>
+              <span class="design design--5"></span>
+              <span class="design design--6"></span>
+              <span class="design design--7"></span>
+              <span class="design design--8"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flip-card-container">
+        <div class="flip-card">
+          <div class="card-front">
+            <figure>
+              <div class="img-bg"></div>
+              <img src={require('../../images/FoodVision.jpg')} alt="Food Vision"/>
+              <figcaption>Food Vision</figcaption>
+            </figure>
+
+            <h3>Technologies:</h3>
+            <p>Python</p>
+            <p>TensorFlow</p>
+            <p>Image Classification</p>
+            <p>Numpy</p>
+            <p>Pandas</p>
+            <p>Matplotlib</p>
+          </div>
+
+          <div class="card-back">
+            <figure>
+              <div class="img-bg"></div>
+              <img src={require('../../images/FoodVision.jpg')} alt="FV2"/>
+            </figure>
+
+            <a href='https://colab.research.google.com/drive/1hYjZupJ2I-Qz-9LzE3sQhbVZzZhvD3qJ?usp=sharing'>
+              <button>Link to Colab notebook</button>
+            </a>
+            <a href='https://data.vision.ee.ethz.ch/cvl/datasets_extra/food-101/static/bossard_eccv14_food-101.pdf'>
+              <button className='top-buffer'>Original Food101 Paper</button>
+            </a>
+
+            <div class="design-container">
+              <span class="design design--1"></span>
+              <span class="design design--2"></span>
+              <span class="design design--3"></span>
+              <span class="design design--4"></span>
+              <span class="design design--5"></span>
+              <span class="design design--6"></span>
+              <span class="design design--7"></span>
+              <span class="design design--8"></span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="flip-card-container">
+        <div class="flip-card">
+          <div class="card-front">
+            <figure>
+              <div class="img-bg"></div>
+              <img src={require('../../images/NLP.jpg')} alt="NLP"/>
+              <figcaption>A Study of NLP Fundamentals</figcaption>
+            </figure>
+
+            <h3>Technologies:</h3>
+            <p>Python</p>
+            <p>TensorFlow</p>
+            <p>Natural Language Processing</p>
+            <p>Numpy</p>
+            <p>Pandas</p>
+            <p>Matplotlib</p>
+          </div>
+
+          <div class="card-back">
+            <figure>
+              <div class="img-bg"></div>
+              <img src={require('../../images/NLP.jpg')} alt="nlp2"/>
+            </figure>
+
+            <a href='https://colab.research.google.com/drive/19hRGH4_cMkotESpTlEA4meykEU9Xhmvl?usp=sharing'>
+              <button>Link to Colab notebook</button>
+            </a>
+            <a href='https://tensorboard.dev/experiment/Ni99Ikj8TmarzNakAh02Sw/#scalars&runSelectionState=eyJtb2RlbF8xX2RlbnNlLzIwMjMwNzA1LTE4NDA1OS90cmFpbiI6dHJ1ZSwibW9kZWxfMl9MU1RNLzIwMjMwNzA1LTE4NDEyMy90cmFpbiI6ZmFsc2UsIm1vZGVsXzNfR1JVLzIwMjMwNzA1LTE4NDI1MS90cmFpbiI6ZmFsc2UsIm1vZGVsXzRfQmlkaXJlY3Rpb25hbC8yMDIzMDcwNS0xODQ0MjQvdHJhaW4iOmZhbHNlLCJtb2RlbF81X0NvbnYxRC8yMDIzMDcwNS0xODQ2NTgvdHJhaW4iOmZhbHNlfQ%3D%3D'>
+              <button className='top-buffer'>Results on TensorBoard</button>
+            </a>
+
+            <div class="design-container">
+              <span class="design design--1"></span>
+              <span class="design design--2"></span>
+              <span class="design design--3"></span>
+              <span class="design design--4"></span>
+              <span class="design design--5"></span>
+              <span class="design design--6"></span>
+              <span class="design design--7"></span>
+              <span class="design design--8"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+
+
+
+      <div className="row justify-content-center">
+      <h2 className='text-center'>GameDev Projects</h2>
+      <div class="flip-card-container">
+        <div class="flip-card">
+          <div class="card-front">
+            <figure>
+              <div class="img-bg"></div>
+              <img src={require('../../images/BoostRocket.png')} alt="Boost Rocket"/>
+              <figcaption>Boost Rocket</figcaption>
+            </figure>
+
+            <h3>Technologies:</h3>
+            <p>Unity Version 2021.3.18f1</p>
+            <p>C#</p>
+            <p>OOP</p>
+          </div>
+
+          <div class="card-back">
+            <figure>
+              <div class="img-bg"></div>
+              <img src={require('../../images/Boost2.png')} alt="Boost Rocket2"/>
+            </figure>
+
+            <a href='https://sharemygame.com/@jon0908776/boostrocket'>
+              <button>Play it here!</button>
+            </a>
+            <a href='https://github.com/JWMedeiros/Unity_Games/tree/master/BoostRocket/Assets'>
+              <button className='top-buffer'>Asset Library</button>
+            </a>
+
+            <div class="design-container">
+              <span class="design design--1"></span>
+              <span class="design design--2"></span>
+              <span class="design design--3"></span>
+              <span class="design design--4"></span>
+              <span class="design design--5"></span>
+              <span class="design design--6"></span>
+              <span class="design design--7"></span>
+              <span class="design design--8"></span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="flip-card-container">
+        <div class="flip-card">
+          <div class="card-front">
+            <figure>
+              <div class="img-bg"></div>
+              <img src={require('../../images/GunGame.png')} alt="Gun Game"/>
+              <figcaption>GunGame</figcaption>
+            </figure>
+
+            <h3>Technologies:</h3>
+            <p>Unity Version 2021.3.18f1</p>
+            <p>C#</p>
+            <p>OOP</p>
+          </div>
+
+          <div class="card-back">
+            <figure>
+              <div class="img-bg"></div>
+              <img src={require('../../images/Gun2.jpg')} alt="GunGame2"/>
+            </figure>
+
+            <a href='https://sharemygame.com/@jon0908776/gungame-1-0'>
+              <button>Play it here!</button>
+            </a>
+            <a href='https://github.com/JWMedeiros/Unity_Games/tree/master/GunGame/Assets'>
+              <button className='top-buffer'>Asset Library</button>
+            </a>
+
+            <div class="design-container">
+              <span class="design design--1"></span>
+              <span class="design design--2"></span>
+              <span class="design design--3"></span>
+              <span class="design design--4"></span>
+              <span class="design design--5"></span>
+              <span class="design design--6"></span>
+              <span class="design design--7"></span>
+              <span class="design design--8"></span>
+            </div>
+          </div>
         </div>
       </div>
       </div>
